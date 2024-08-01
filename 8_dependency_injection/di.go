48cc5fcc6a441e0c3695edf80482cc865dceb4e5
dfneto <pdavidfneto@gmail.com>
2024-08-01 08:59:53 +0200

    contudo, se queremos imprimir na saída padrão não podemos

--- 8_dependency_injection/di.go
+++ 8_dependency_injection/di.go
@@ -3,8 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 func Greet(buffer *bytes.Buffer, name string) {
 	fmt.Fprintf(buffer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
