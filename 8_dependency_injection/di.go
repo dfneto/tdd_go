660465b3e90fd9846060075472720413bb586d86
dfneto <pdavidfneto@gmail.com>
2024-08-01 09:23:02 +0200

    também poderíamos usar Greet para receber um ResponseWriter, já que este implementa um Writer

--- 8_dependency_injection/di.go
+++ 8_dependency_injection/di.go
@@ -13,3 +13,11 @@ func Greet(writer io.Writer, name string) {
 func main() {
 	Greet(os.Stdout, "Elodie")
 }
+
+// func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+// 	Greet(w, "world")
+// }
+
+// func main() {
+// 	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+// }
