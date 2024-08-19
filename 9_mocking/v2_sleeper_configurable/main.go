074740df1a91ea498e13fdb91abed5d265630e3d
dfneto <pdavidfneto@gmail.com>
2024-08-19 19:04:24 +0200

    refactor

---
+++ 9_mocking/v2_sleeper_configurable/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+// ConfigurableSleeper is an implementation of Sleeper with a defined delay.
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+// Sleep will pause execution for the defined Duration.
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	// sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Millisecond, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
